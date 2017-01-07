a5b9a36afcb59a24ecde6792791e87cfcae6c0e4
satyajyoti1998 <satyajyoti.nanda@gmail.com>
2017-01-07 09:56:32 -0500

    added number input option

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main()
-{
+{	
+	int x = 50;
 	printf("\nHello world!");
+	printf("\nPlease enter a number: ");
+	scanf("%d", &x);
+	printf("\nYour number is: %d", x);
+
 	return 0;
 }
